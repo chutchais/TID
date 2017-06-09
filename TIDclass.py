509456e0faaa384a0429bd9a91ba88148206f14a
unknown <Chutchai@NB_SecMGR.lcb1.com>
2017-06-09 16:47:21 +0700

    Add new 2 platform

--- TIDclass.py
+++ TIDclass.py
@@ -27,7 +27,6 @@ class tid:
 				i=i+1
 		return ''
 
-
 	def getInfo(self):
 		fp = open(self.filename, 'rb')
 		parser = PDFParser(fp)
@@ -47,181 +46,192 @@ class tid:
 		rex = re.compile("^[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]$")
 
 		for page in doc.get_pages():
-		    interpreter.process_page(page)
-		    layout = device.get_result()
-		    layout_count = self.get_layout_count(layout)
-		    i=1
+			interpreter.process_page(page)
+			layout = device.get_result()
+			layout_count = self.get_layout_count(layout)
+			i=1
 
-		    # print ('LayOut Count : %s' % layout_count)
-		    # if layout_count =9 , means Full1Box (IN) or MTY1Box(OUT)
-		    company=self.get_company(layout,layout_count)
-		    lpn = self.get_license_plate_number(layout,layout_count)
-		    
+			# print ('LayOut Count : %s' % layout_count)
+			# if layout_count =9 , means Full1Box (IN) or MTY1Box(OUT)
+			company=self.get_company(layout,layout_count)
+			lpn = self.get_license_plate_number(layout,layout_count)
+			
 
-		    contain1_no = self.get_container1(layout,layout_count)
-		    contain1_loc =  self.get_location1(layout,layout_count)
-		    contain1_seal = self.get_seal_number1(layout,layout_count)
+			contain1_no = self.get_container1(layout,layout_count)
+			contain1_loc =  self.get_location1(layout,layout_count)
+			contain1_seal = self.get_seal_number1(layout,layout_count)
 
-		    contain2_no = self.get_container2(layout,layout_count)
-		    contain2_loc =  self.get_location2(layout,layout_count)
-		    contain2_seal = self.get_seal_number2(layout,layout_count)
-
-
-		    call_card = self.get_call_card(layout,layout_count)
-		    time_stamp =self.get_timestamp(layout,layout_count)
-
-		    data = {
-		    		'filename' : self.filename,
-		    		'type' : layout_count,
-		    		'company' : company ,
-		    		'license_no': lpn,
-		    		'call_card':call_card,
-		    		'time_stamp' : time_stamp
-		    		}
+			contain2_no = self.get_container2(layout,layout_count)
+			contain2_loc =  self.get_location2(layout,layout_count)
+			contain2_seal = self.get_seal_number2(layout,layout_count)
 
 
+			call_card = self.get_call_card(layout,layout_count)
+			time_stamp =self.get_timestamp(layout,layout_count)
 
-		    if layout_count == 9 :
-		    	# print("Full (IN)/Empty(Out) 1 Box TID")
-		    	c2_text = self.get_line_text(layout,2)
-
-		    	if len(c2_text) > 1 :
-		    		direction='in'
-		    		# print ('Full (IN) 1 box')
-		    	else:
-		    		direction='out'
-		    		# print ('Full (OUT) 1 box')
-
-		    	if direction=='in':
-			    	data[direction]={}
-			    	data[direction]['item1']={'container_no' : contain1_no,
-			    				'location' : contain1_loc,
-			    				'seal' : contain1_seal}
-
-		    	if direction=='out':
-		    		# either 1 or 2 Full out
-		    		c4_text = self.get_line_text(layout,4)
-		    		y= c4_text.split('  ')
-			    	data[direction]={}
-			    	if '-' in c4_text :
-			    		data[direction]['item1']={'container_no' : contain1_no,
-			    				'location' : contain1_loc,
-			    				'seal' : contain1_seal,
-			    				'line3':''}
-			    	else:
-			    		data[direction]['item1']={'container_no' : contain1_no,
-			    				'location' : contain1_loc,
-			    				'seal' : contain1_seal,
-			    				'line3' : ''}
-			    		data[direction]['item2']={'container_no' : contain2_no,
-			    				'location' : contain2_loc,
-			    				'seal' : contain2_seal,
-			    				'line3' : ''}
+			data = {
+					'filename' : self.filename,
+					'type' : layout_count,
+					'company' : company ,
+					'license_no': lpn,
+					'call_card':call_card,
+					'time_stamp' : time_stamp
+					}
 
 
-			    	# data[direction]['item1']={'container_no' : contain1_no,
-			    	# 			'location' : contain1_loc,
-			    	# 			'seal' : contain1_seal}
-		    	
-		    if layout_count == 7 :
-		    	data['in']={}
-		    	data['in']['item1']={'container_no' : contain1_no,
-		    				'location' : contain1_loc,
-		    				'seal' : contain1_seal}
-		    	data['in']['item2']={'container_no' : contain2_no,
-		    				'location' : contain2_loc,
-		    				'seal' : contain2_seal}
 
-		    if layout_count == 10 :
-		    	data['in']={}
-		    	data['in']['item1']={'container_no' : contain1_no,
-		    				'location' : contain1_loc,
-		    				'seal' : contain1_seal}
+			if layout_count == 9 :
+				# print("Full (IN)/Empty(Out) 1 Box TID")
+				c2_text = self.get_line_text(layout,2)
 
+				if len(c2_text) > 1 :
+					direction='in'
+					# print ('Full (IN) 1 box')
+				else:
+					direction='out'
+					# print ('Full (OUT) 1 box')
 
-		    	tmp=self.get_line3text1(layout,layout_count)
-		    	if rex.match(tmp):
-		    		location=tmp
-		    	else:
-		    		location=''
+				if direction=='in':
+					data[direction]={}
+					data[direction]['item1']={'container_no' : contain1_no,
+								'location' : contain1_loc,
+								'seal' : contain1_seal}
 
-		    	data['out']={}
-		    	data['out']['item1']={
-		    				'container_no' : self.get_service1(layout,layout_count),
-		    				'seal' : self.get_container1_type(layout,layout_count),
-		    				'line3' : self.get_line3text1(layout,layout_count),
-		    				'location' : location}
-
-		    	tmp=self.get_line3text2(layout,layout_count)
-		    	if rex.match(tmp):
-		    		location=tmp
-		    	else:
-		    		location=''
-
-		    	data['out']['item2']={
-		    				'container_no' : self.get_service2(layout,layout_count),
-		    				'seal' : self.get_container2_type(layout,layout_count),
-		    				'line3' : self.get_line3text2(layout,layout_count),
-		    				'location' : location}
-
-		    if layout_count == 11 :
-		    	data['out']={}
-		    	tmp=self.get_line3text1(layout,layout_count)
-		    	if rex.match(tmp):
-		    		location=tmp
-		    	else:
-		    		location=''
-
-		    	data['out']['item1']={
-		    	'container_no' : self.get_service1(layout,layout_count),
-		    	'seal' : self.get_container1_type(layout,layout_count),
-		    	'line3' : self.get_line3text1(layout,layout_count),
-		    	'location' : location
-		    	}
-
-		    	tmp=self.get_line3text2(layout,layout_count)
-		    	if rex.match(tmp):
-		    		location=tmp
-		    	else:
-		    		location=''
-
-		    	data['out']['item2']={
-		    				'container_no' : self.get_service2(layout,layout_count),
-		    				'seal' : self.get_container2_type(layout,layout_count),
-		    				'line3' : self.get_line3text2(layout,layout_count),
-		    				'location' : location}
+				if direction=='out':
+					# either 1 or 2 Full out
+					c4_text = self.get_line_text(layout,4)
+					y= c4_text.split('  ')
+					data[direction]={}
+					if '-' in c4_text :
+						data[direction]['item1']={'container_no' : contain1_no,
+								'location' : contain1_loc,
+								'seal' : contain1_seal,
+								'line3':''}
+					else:
+						data[direction]['item1']={'container_no' : contain1_no,
+								'location' : contain1_loc,
+								'seal' : contain1_seal,
+								'line3' : ''}
+						data[direction]['item2']={'container_no' : contain2_no,
+								'location' : contain2_loc,
+								'seal' : contain2_seal,
+								'line3' : ''}
 
 
-		    if layout_count == 12 :
-		    	data['out']={}
-		    	tmp=self.get_line3text1(layout,layout_count)
-		    	if rex.match(tmp):
-		    		location=tmp
-		    	else:
-		    		location=''
+					# data[direction]['item1']={'container_no' : contain1_no,
+					# 			'location' : contain1_loc,
+					# 			'seal' : contain1_seal}
+				
+			if layout_count == 7 :
+				data['in']={}
+				data['in']['item1']={'container_no' : contain1_no,
+							'location' : contain1_loc,
+							'seal' : contain1_seal}
 
-		    	data['out']['item1']={
-		    				'container_no' : self.get_service1(layout,layout_count),
-		    				'seal' : self.get_container1_type(layout,layout_count),
-		    				'line3' : self.get_line3text1(layout,layout_count),
-		    				'location' :location}
-
-		    	tmp=self.get_line3text2(layout,layout_count)
-		    	if rex.match(tmp):
-		    		location=tmp
-		    	else :
-		    		location=''
-
-		    	data['out']['item2']={
-		    				'container_no' : self.get_service2(layout,layout_count),
-		    				'seal' : self.get_container2_type(layout,layout_count),
-		    				'line3' : self.get_line3text2(layout,layout_count),
-		    				'location' :location}
+				if self.get_line_text(layout,3) == '-' :
+					data['in']['item2']={'container_no' : contain2_no,
+					'location' : contain2_loc,
+					'seal' : contain2_seal}
+				else:
+					data['out']={}
+					data['out']['item1'] ={'container_no':'',
+						'location' : '',
+						'seal':''}
+						# print('')
 
 
-		    return data
-		    	# For View file details
-		    # if argv[1] == 'y':
+
+			if layout_count == 10 :
+				data['in']={}
+				data['in']['item1']={'container_no' : contain1_no,
+							'location' : contain1_loc,
+							'seal' : contain1_seal}
+
+
+				tmp=self.get_line3text1(layout,layout_count)
+				if rex.match(tmp):
+					location=tmp
+				else:
+					location=''
+
+				data['out']={}
+				data['out']['item1']={
+							'container_no' : self.get_service1(layout,layout_count),
+							'seal' : self.get_container1_type(layout,layout_count),
+							'line3' : self.get_line3text1(layout,layout_count),
+							'location' : location}
+
+				tmp=self.get_line3text2(layout,layout_count)
+				if rex.match(tmp):
+					location=tmp
+				else:
+					location=''
+
+				data['out']['item2']={
+							'container_no' : self.get_service2(layout,layout_count),
+							'seal' : self.get_container2_type(layout,layout_count),
+							'line3' : self.get_line3text2(layout,layout_count),
+							'location' : location}
+
+			if layout_count == 11 :
+				data['out']={}
+				tmp=self.get_line3text1(layout,layout_count)
+				if rex.match(tmp):
+					location=tmp
+				else:
+					location=''
+
+				data['out']['item1']={
+				'container_no' : self.get_service1(layout,layout_count),
+				'seal' : self.get_container1_type(layout,layout_count),
+				'line3' : self.get_line3text1(layout,layout_count),
+				'location' : location
+				}
+
+				if self.get_line_text(layout,3)=='-' :
+					tmp=self.get_line3text2(layout,layout_count)
+					if rex.match(tmp):
+						location=tmp
+					else:
+						location=''
+
+					data['out']['item2']={
+								'container_no' : self.get_service2(layout,layout_count),
+								'seal' : self.get_container2_type(layout,layout_count),
+								'line3' : self.get_line3text2(layout,layout_count),
+								'location' : location}
+
+
+			if layout_count == 12 :
+				data['out']={}
+				tmp=self.get_line3text1(layout,layout_count)
+				if rex.match(tmp):
+					location=tmp
+				else:
+					location=''
+
+				data['out']['item1']={
+							'container_no' : self.get_service1(layout,layout_count),
+							'seal' : self.get_container1_type(layout,layout_count),
+							'line3' : self.get_line3text1(layout,layout_count),
+							'location' :location}
+
+				tmp=self.get_line3text2(layout,layout_count)
+				if rex.match(tmp):
+					location=tmp
+				else :
+					location=''
+
+				data['out']['item2']={
+							'container_no' : self.get_service2(layout,layout_count),
+							'seal' : self.get_container2_type(layout,layout_count),
+							'line3' : self.get_line3text2(layout,layout_count),
+							'location' :location}
+
+
+			return data
+				# For View file details
+			# if argv[1] == 'y':
 			   #  for lt_obj in layout:
 			   #      if isinstance(lt_obj, LTTextBox) :
 			   #      	line_text=lt_obj.get_text()
@@ -230,7 +240,7 @@ class tid:
 			   #      	i=i+1
 
 
-		    # print('-------------End Page----------------------------------------------')
+			# print('-------------End Page----------------------------------------------')
 	
 	def getRaw(self):
 		fp = open(self.filename, 'rb')
@@ -274,10 +284,23 @@ class tid:
 		# 	return x[0].strip()
 
 	def get_license_plate_number(self,layout,tid_type):
-		if tid_type == 9 or tid_type == 7 or tid_type == 11 or tid_type == 10:
+		if tid_type == 9 or tid_type == 7 or tid_type == 10:
 			line_text = self.get_line_text(layout,1)
 			x = line_text.split('    ')
 			return x[len(x)-1].strip().replace(' ','')
+
+
+		if tid_type == 11 :
+			line_text = self.get_line_text(layout,3) #Check 
+			if line_text == '-':
+				line_text = self.get_line_text(layout,1)
+				x = line_text.split('    ')
+				return x[len(x)-1].strip().replace(' ','')
+			else:
+				line_text = self.get_line_text(layout,2)
+				return line_text.strip().replace(' ','')
+
+
 
 		if tid_type == 12 :
 			line_text = self.get_line_text(layout,2)
@@ -448,10 +471,21 @@ class tid:
 
 			
 	def get_service1(self,layout,tid_type):
-		if tid_type == 10 or tid_type == 11 :
+		if tid_type == 10 :
 			line_text = self.get_line_text(layout,2).strip()
 			x = line_text.split('    ')
 			return x[0].strip().replace(' ','')
+
+		if tid_type == 11 :
+			line_text = self.get_line_text(layout,3).strip()
+			if line_text=='-':
+				line_text = self.get_line_text(layout,2).strip()
+				x = line_text.split('    ')
+				return x[0].strip().replace(' ','')
+			else:
+				line_text = self.get_line_text(layout,3).strip()
+				return line_text.strip().replace(' ','')
+
 
 		if tid_type == 12 :
 			line_text = self.get_line_text(layout,3).strip()
@@ -479,11 +513,20 @@ class tid:
 			return '%s  %s' % (x[0].strip().replace(' ',''),y[0].strip().replace(' ','').replace('DV',' DV '))
 
 		if tid_type == 11 :
-			line_text1 = self.get_line_text(layout,5).strip()
-			x = line_text1.split('    ')
-			line_text2 = self.get_line_text(layout,6).strip()
-			y = line_text2.split('    ')
-			return '%s  %s' % (x[0].strip().replace(' ',''),y[0].strip().replace(' ','').replace('DV',' DV '))
+			line_text = self.get_line_text(layout,3).strip()
+			if line_text=='-':
+				line_text1 = self.get_line_text(layout,5).strip()
+				x = line_text1.split('    ')
+				line_text2 = self.get_line_text(layout,6).strip()
+				y = line_text2.split('    ')
+				return '%s  %s' % (x[0].strip().replace(' ',''),y[0].strip().replace(' ','').replace('DV',' DV '))
+			else:
+				line_text1 = self.get_line_text(layout,6).strip()
+				x = line_text1.split('    ')
+				line_text2 = self.get_line_text(layout,7).strip()
+				y = line_text2.split('    ')
+				return '%s  %s' % (x[0].strip().replace(' ',''),y[0].strip().replace(' ','').replace('DV',' DV '))
+
 
 		if tid_type == 12 :
 			line_text1 = self.get_line_text(layout,6).strip()
@@ -549,46 +592,46 @@ class tid:
 
 
 	def _parse_pages (doc, images_folder):
-	    """With an open PDFDocument object, get the pages, parse each one, and return the entire text
-	    [this is a higher-order function to be passed to with_pdf()]"""
-	    rsrcmgr = PDFResourceManager()
-	    laparams = LAParams()
-	    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-	    interpreter = PDFPageInterpreter(rsrcmgr, device)
+		"""With an open PDFDocument object, get the pages, parse each one, and return the entire text
+		[this is a higher-order function to be passed to with_pdf()]"""
+		rsrcmgr = PDFResourceManager()
+		laparams = LAParams()
+		device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+		interpreter = PDFPageInterpreter(rsrcmgr, device)
 
-	    text_content = [] # a list of strings, each representing text collected from each page of the doc
-	    for i, page in enumerate(doc.get_pages()):
-	        interpreter.process_page(page)
-	        # receive the LTPage object for this page
-	        layout = device.get_result()
-	        # layout is an LTPage object which may contain child objects like LTTextBox, LTFigure, LTImage, etc.
-	        text_content.append(parse_lt_objs(layout.objs, (i+1), images_folder))
+		text_content = [] # a list of strings, each representing text collected from each page of the doc
+		for i, page in enumerate(doc.get_pages()):
+			interpreter.process_page(page)
+			# receive the LTPage object for this page
+			layout = device.get_result()
+			# layout is an LTPage object which may contain child objects like LTTextBox, LTFigure, LTImage, etc.
+			text_content.append(parse_lt_objs(layout.objs, (i+1), images_folder))
 
-	    return text_content
+		return text_content
 
 	def parse_lt_objs (lt_objs, page_number, images_folder, text=[]):
-	    """Iterate through the list of LT* objects and capture the text or image data contained in each"""
-	    text_content = [] 
+		"""Iterate through the list of LT* objects and capture the text or image data contained in each"""
+		text_content = [] 
 
-	    for lt_obj in lt_objs:
-	        if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
-	            # text
-	            text_content.append(lt_obj.get_text())
-	            print(lt_obj.get_text())
-	        elif isinstance(lt_obj, LTImage):
-	            # an image, so save it to the designated folder, and note it's place in the text 
-	            saved_file = save_image(lt_obj, page_number, images_folder)
-	            if saved_file:
-	                # use html style <img /> tag to mark the position of the image within the text
-	                text_content.append('<img src="'+os.path.join(images_folder, saved_file)+'" />')
-	            else:
-	                print >> sys.stderr, "Error saving image on page", page_number, lt_obj.__repr__
-	        elif isinstance(lt_obj, LTFigure):
-	            # LTFigure objects are containers for other LT* objects, so recurse through the children
-	            text_content.append(parse_lt_objs(lt_obj.objs, page_number, images_folder, text_content))
+		for lt_obj in lt_objs:
+			if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
+				# text
+				text_content.append(lt_obj.get_text())
+				print(lt_obj.get_text())
+			elif isinstance(lt_obj, LTImage):
+				# an image, so save it to the designated folder, and note it's place in the text 
+				saved_file = save_image(lt_obj, page_number, images_folder)
+				if saved_file:
+					# use html style <img /> tag to mark the position of the image within the text
+					text_content.append('<img src="'+os.path.join(images_folder, saved_file)+'" />')
+				else:
+					print >> sys.stderr, "Error saving image on page", page_number, lt_obj.__repr__
+			elif isinstance(lt_obj, LTFigure):
+				# LTFigure objects are containers for other LT* objects, so recurse through the children
+				text_content.append(parse_lt_objs(lt_obj.objs, page_number, images_folder, text_content))
 
-	    return text_content
-	    # return '\n'.join(text_content)
+		return text_content
+		# return '\n'.join(text_content)
 
 	# if __name__ == "__main__":
 	#     sys.exit(main(sys.argv[1:]))
